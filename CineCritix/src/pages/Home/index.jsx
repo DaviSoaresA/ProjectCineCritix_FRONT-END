76d40079e772b8dabee77f3b70eebf305e066f48
Rodrigo5431 <rodrigokarvalho12890@gmail.com>
2025-01-08 21:16:54 -0300

    requisicao pelo front em api externa

--- CineCritix/src/pages/Home/index.jsx
+++ CineCritix/src/pages/Home/index.jsx
@@ -1,12 +1,59 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import * as styles from "../Home/Home.module.css";
 
-export default function Home(){
-    return(
-        <div className={styles.container}>
-            <div>
-                <h1>Home</h1>
-                <p>Welcome to CineCritix!</p>
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
+
+  async function getMovies() {
+    try {
+      const response = await axios.get(
+        `http://www.omdbapi.com/?s=movie&page=${page}&apikey=2fcfe92f`
+      );
+
+      if (response.data.Response === "True") {
+        setMovies((prevMovies) => [...prevMovies, ...response.data.Search]);
+      }
+    } catch (error) {
+      console.error("Error fetching movies:", error);
+    }
+  }
+
+  useEffect(() => {
+    getMovies();
+  }, [page]);
+
+  return (
+    <div className={styles.container}>
+      <div>
+        <h1>Movies</h1>
+        <p>Welcome to CineCritix!</p>
+        {movies.length > 0 ? (
+          movies.map((movie) => (
+            <div key={movie.imdbID} className={styles.movieCard}>
+              <h2>{movie.Title}</h2>
+              <img
+                src={movie.Poster}
+                onError={(e) => (e.target.src = "/placeholder.png")}
+                alt={`Cover of the movie ${movie.Title}`}
+                style={{ width: "200px", height: "auto", marginBottom: "10px" }}
+              />
+              <p>
+                <strong>Year:</strong> {movie.Year}
+              </p>
+              <p>
+                <strong>Type:</strong> {movie.Type}
+              </p>
             </div>
-        </div>
-    )
+          ))
+        ) : (
+          <p>Loading movies...</p>
+        )}
+        <button onClick={() => setPage((prevPage) => prevPage + 1)}>
+          Load More
+        </button>
+      </div>
+    </div>
+  );
 }
