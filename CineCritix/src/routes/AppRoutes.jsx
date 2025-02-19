d77f0b4ca57fdb252799c0bced5a87cc1d435558
pckzin01 <patrickgoncalvesdepaiva@gmail.com>
2025-02-19 14:11:52 -0300

    my account iniciada

--- CineCritix/src/routes/AppRoutes.jsx
+++ CineCritix/src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Home from '../pages/Home'
 import Movie from '../pages/Movie'
 import { Route, Router, Routes } from 'react-router-dom'
 import Register from '../pages/Register'
+import MyAccount from '../pages/MyAccount'
 
 export default function AppRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function AppRoutes() {
         <Route path="/" element={<Home />} />
         <Route path="/movie/:id" element={<Movie />} />
         <Route path="/cadastrar" element={<Register />} />
+        <Route path="/minhaConta" element={<MyAccount />} />
       </Routes>
   )
 }
